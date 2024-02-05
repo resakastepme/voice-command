3f39eac2a201553a1852dfb3e855459efb14ee05
resakastepme <resa.komara.akbari@gmail.com>
2024-02-05 15:32:57 +0700

    CLIENT SIDE FINISH

---
+++ public/js/main.js
@@ -0,0 +1,111 @@
+const recordBtn = document.querySelector("#record"),
+    result = document.querySelector("#result")
+
+let SpeechRecognition =
+    window.SpeechRecognition || window.webkitSpeechRecognition,
+    recognition,
+    recording = false;
+
+function soundcloud(text) {
+
+    $.ajax({
+        url: windows.location.origin + '',
+        type: 'POST',
+        data: {
+            text: text
+        }, success: function (response) {
+            $('#iframe').html('');
+            $('#iframe').html('<iframe width="100%" height="500" scrolling="no" frameborder="no" allow="autoplay"\
+            src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/'+ response + '&amp;auto_play=true">\
+            </iframe>');
+        }
+    })
+}
+
+function speechToText() {
+    try {
+        recognition = new SpeechRecognition();
+        recognition.lang = 'id';
+        recognition.interimResults = true;
+        recordBtn.classList.add("recording");
+        recordBtn.querySelector("p").innerHTML = "Mendengar...";
+        $('#mendengarkanIcon').show();
+        $('#standbyIcon').hide();
+        $('#hiddenOutput').val('');
+        recognition.start();
+        recognition.onresult = (event) => {
+            const speechResult = event.results[0][0].transcript;
+            $('#result').html('');
+            result.innerHTML += " " + speechResult;
+            $('#hiddenOutput').val(speechResult);
+            downloadBtn.disabled = false;
+        };
+        recognition.onspeechend = () => {
+            speechToText();
+        };
+        recognition.onerror = (event) => {
+            stopRecording();
+            if (event.error === "no-speech") {
+                console.log("Tidak ada kata terdengar, berhenti mendengar...");
+                $('#result').html('');
+                if ($('#hiddenOutput').val() != '') {
+                    soundcloud($('#hiddenOutput').val());
+                }
+                $('#standbyIcon').show();
+                $('#mendengarkanIcon').hide();
+            } else if (event.error === "audio-capture") {
+                alert("Microphone tidak terdeteksi.");
+                $('#result').html('');
+                $('#standbyIcon').show();
+                $('#mendengarkanIcon').hide();
+            } else if (event.error === "not-allowed") {
+                alert("Akses microphone tidak diberikan.");
+                $('#result').html('');
+                $('#standbyIcon').show();
+                $('#mendengarkanIcon').hide();
+            } else if (event.error === "aborted") {
+                console.log('Berhenti mendengar..');
+                $('#result').html('');
+                if ($('#hiddenOutput').val() != '') {
+                    soundcloud($('#hiddenOutput').val());
+                }
+                $('#standbyIcon').show();
+                $('#mendengarkanIcon').hide();
+            } else {
+                alert("Error: " + event.error);
+                $('#result').html('');
+                $('#standbyIcon').show();
+                $('#mendengarkanIcon').hide();
+            }
+        };
+    } catch (error) {
+        recording = false;
+
+        console.log(error);
+
+        $('#standbyIcon').show();
+        $('#mendengarkanIcon').hide();
+    }
+}
+
+// $(document).ready(function () {
+//     setTimeout( ()=>{
+//         $('#record').click();
+//     }, 1000)
+// })
+
+recordBtn.addEventListener("click", () => {
+    if (!recording) {
+        speechToText();
+        recording = true;
+    } else {
+        stopRecording();
+    }
+});
+
+function stopRecording() {
+    recognition.stop();
+    recordBtn.querySelector("p").innerHTML = "Mulai mendegar";
+    recordBtn.classList.remove("recording");
+    recording = false;
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="{{ asset('favicon.png') }}" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.1/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <title> VOICE COMMAND </title>
+</head>
+
+<body>
+
+    <div class="container mt-5 mb-5">
+
+        <h3 id="titleHeader"> CARI LAGU APA? </h3>
+        {{-- <button id="button"> click </button> --}}
+        <hr>
+        <button class="btn record" id="record">
+            <div class="icon">
+                <ion-icon name="mic-outline" style="display: block" id="standbyIcon"></ion-icon>
+                <img src="{{ asset('bars.svg') }}" alt="no image" id="mendengarkanIcon" style="display: none; height: 40px;"/>
+            </div>
+            <p class="mt-3 ms-2">Mulai dengarkan</p>
+        </button>
+
+        <h4 class="mt-5 mb-5" spellcheck="false" id="result"></h4>
+        <input type="hidden" id="hiddenOutput">
+
+        <small style="font-size: 11px; font-style: italic;"> saran: </small>
+        <ul>
+            <li style="font-size: 13px;"> putar lagu justin bieber baby </li>
+        </ul>
+
+    </div>
+
+    <section id="iframe">
+
+    </section>
+
+</body>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.1/js/bootstrap.min.js"></script>
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+<script type="text/javascript" src="{{ asset('js/main.js') }}"></script>
+
+</html>
